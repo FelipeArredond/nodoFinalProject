55d3befe008e178781d840677a34576ac1d03e57
danielmao21 <danielortizdavid@gmail.com>
2023-02-10 19:52:02 -0500

    testimonial routing add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+      path:'testimonial',
+      component: TestimonialsComponent,
+
   },
   {
     path: 'signup',
