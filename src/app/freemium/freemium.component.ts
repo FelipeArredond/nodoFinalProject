87215da84836c7bba212f316a2d689226ccc015e
FelipeArredond <pipechiqui77@gamil.com>
2023-02-16 10:17:17 -0500

    Users can inscribe to course now

--- src/app/freemium/freemium.component.ts
+++ src/app/freemium/freemium.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserDataServiceService } from '../user-data-service.service';
 import { Router } from '@angular/router';
+import { ICourseItem } from './ICourseItem';
 import { IUserData } from '../login/IUserData';
 
 @Component({
@@ -10,19 +11,45 @@ import { IUserData } from '../login/IUserData';
 })
 export class FreemiumComponent {
 
+  courses: ICourseItem[]  = [];
+
+  userData = {
+    username: '',
+    password: ''
+  }
+
   constructor(private userDataService: UserDataServiceService, private router: Router){}
 
   navigate(path: string){
     this.router.navigate(['/', path])
   }
 
-  validateInscribe(){
+  async validateInscribe(id: number, courseImageURL: string, courseTittle: string, courseDescription: string, courseHours: number, courseUsers: string[]){
     if(this.userDataService.getUserData().isLogged){
+      await fetch(`http://localhost:3000/courses/${id}`,{
+        method: 'PUT',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          imageURL: courseImageURL,
+          tittle: courseTittle,
+          desc: courseDescription,
+          hours: courseHours,
+          users: [...courseUsers, this.userDataService.getUserData().username]
+        })
+      }).then(res => console.log(res))
       alert('Inscrito')
     }else{
       alert('Debes de Iniciar Sesion')
       this.navigate('login');
     }
+  }
+
+  async ngOnInit(){
+    const res = await fetch("http://localhost:3000/courses");
+    const data = await res.json();
+    this.courses = data;
   }
 
 }
